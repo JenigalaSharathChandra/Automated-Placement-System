5f7ac3b5a7d47fe9b0eaeada4064c194adc01313
Jenigala Sharath Chandra <sharathchandra.jenigala@gmail.com>
2025-06-15 21:45:57 +0530

    landing page updated

--- campus_runner/urls.py
+++ campus_runner/urls.py
@@ -20,37 +20,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-from django.core.management import call_command
-from django.contrib.auth import get_user_model
-from django.views.decorators.csrf import csrf_exempt  # ✅ Import added
+ # ✅ Import added
 
-@csrf_exempt
-def create_superuser(request):
-    User = get_user_model()
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create_superuser(
-            username='245122750306',
-            email='245122750306@mvsrec.edu.in',
-            password='Jbunny@123'
-        )
-        return HttpResponse("✅ Superuser '245122750306' created with password 'Jbunny@123'.")
-    return HttpResponse("⚠️ Superuser already exists.")
 
-@csrf_exempt
-def run_migrations(request):
-    call_command('migrate')
-    return HttpResponse("✅ Migrations completed successfully!")
-
-@csrf_exempt
-def run_collectstatic(request):
-    call_command('collectstatic', interactive=False)
-    return HttpResponse("✅ Static files collected.")
 
 urlpatterns = [
-    path('create-admin/', create_superuser),
-    path('run-migrations/', run_migrations),
-    path('collectstatic/', run_collectstatic),  # Temporary setup URLs
     path('', include('accounts.urls')),
     path('student/', include('student.urls')),
     path('company/', include('companies.urls')),
